52f091f6a49290c5caeb8a798ce38305e8c13cbf
Mitchell <mz2909@nyu.edu>
2023-09-29 19:04:58 -0400

    excel works

--- tt.py
+++ tt.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from openpyxl import Workbook
 
 
 def grab_sections(html):
@@ -16,9 +17,29 @@ def id_checker(adblocker, regular):
     else:
         for option in section1:
             if option in section2:
-                same = True             # not the best way of doing this !!!!!!! MAY NEED TO FIX LATER
+                same = True  # not the best way of doing this !!!!!!! MAY NEED TO FIX LATER
                 break
     return same, section1, section2
+
+
+def export_data(data, original, adBlocker):
+    workbook = Workbook()
+    sheet = workbook.active
+    sheet[f'A{1}'] = f'Original = {original} <div> tags'
+    sheet[f'A{2}'] = f'AdBlocker = {adBlocker} <div> tags'
+
+    sheet[f'C{1}'] = f"same : {len(data['same'])}"
+    sheet[f'C{2}'] = f"dropped: {len(data['dropped'])}"
+    sheet[f'C{3}'] = f"changed: {len(data['changed'])}"
+
+    columns = [('A', 'same'), ('C','dropped'), ('E', 'changed')]
+    for column, attribute in columns:
+        for i in range(5, len(data[attribute]) + 5):
+            cell = column+str(i)
+            sheet[cell] = ' '.join(data[attribute][i-5])
+
+    workbook.save('output_file.xlsx')
+    workbook.close()
 
 
 def get_difference(adblocker, regular):
@@ -48,33 +69,33 @@ def get_difference(adblocker, regular):
                 data['dropped'].append(ref[2])
                 j += 1
 
-        while i < len(adblocker):
-            data['dropped'].append(str(adblocker[i]))
-            i += 1
+        while j < len(regular):
+            data['dropped'].append(str(regular[j]))
+            j += 1
 
     return data
 
 
 f1 = open("adblocker.txt", "r")
-f1 = grab_sections(''.join(f1.readlines()))
-
 f2 = open("regular.txt", "r")
+
+f1 = grab_sections(''.join(f1.readlines()))
 f2 = grab_sections(''.join(f2.readlines()))
 
 ans = get_difference(f1, f2)
 
-print(len(f1))
-print(len(f2))
+f = open("output.txt", "a")
 
 output = f"""
-
+Original = {len(f2)} <div> tags
+AdBlocker = {len(f1)} <div> tags
+============================================
 same = {len(ans['same'])}
 dropped = {len(ans['dropped'])}
 changed = {len(ans['changed'])}
 =============================================
-
-    
-
-
 """
-print(output)
+export_data(ans, len(f2), len(f1))
+
+
+
