86c9e636f37384310db1cb620d0442ef0416c0a3
Mitchell <mz2909@nyu.edu>
2023-10-25 13:53:17 -0400

    test.pou

--- Current/navigation bar/test.py
+++ Current/navigation bar/test.py
@@ -3,37 +3,72 @@ from selenium.webdriver.common.by import By
 from time import *
 
 driver = webdriver.Chrome()
-
-driver.get("https://en.softonic.com/articles")
+driver.set_window_size(1080,800)
 
 sites = ['https://en.wikipedia.org/wiki/Main_Page',
-         ''
-
-
-
-
-
+         'https://www.amazon.com/',
+         'https://www.microsoft.com/en-us/',
+         'https://www.office.com/',
+         'https://openai.com/',
+         'https://www.bing.com/',
+         'https://duckduckgo.com/',
+         'https://weather.com/',
+         'https://cnn.com',
+         'https://www.nytimes.com/',
+         'https://www.twitch.tv/',
+         'https://www.imdb.com/',
+         'https://mail.ru/',
+         'https://naver.com',
+         'https://zoom.us/',
+         'https://www.globo.com/',
+         'https://www.ebay.com/',
+         'https://www.foxnews.com/',
+         'https://www.instructure.com/',
+         'https://www.walmart.com/',
+         'https://www.indeed.com/',
+         'https://www.paypal.com/us/home',
+         'https://www.accuweather.com/',
+         'https://www.pinterest.com/',
+         'https://www.bbc.com/',
+         'https://www.homedepot.com/',
+         'https://www.breitbart.com/',
+         'https://github.com/'
          ]
 
+tag = ['button',
+       'div',
+       'input',
+       'svg',
+       'a'
+       ]
 
+attributes = ['main menu',
+              'open menu',
+              'all microsoft menu',
+              'menu',
+              'navigation',
+              'hamburger',
+              'false',
+              'true']
 
-
-
-
-
-sleep(3)
-try:
-    menu_button1 = driver.find_element(By.CSS_SELECTOR, '[aria-label="Main menu"]')
-    menu_button2 = driver.find_element(By.XPATH, '//button[@aria-label="Main menu"]')
-
-    # Click the menu button
-    menu_button2.click()
-
-
-except Exception:
-    print("Element with aria-label 'Main menu' not found.")
+for url in sites:
+    worked = False
+    driver.switch_to.window(driver.window_handles[-1])  # Switch to the new tab
+    driver.get(url)
+    sleep(3)
+    for attribute in attributes:
+        xpath = f'//*[translate(@aria-label, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz")="{attribute.lower()}"]'
+        try:
+            found_element = driver.find_element(By.XPATH, xpath)
+            found_element.click()
+            print("clicked on the drop down for", url)
+            worked = True
+            break
+        except Exception:
+            ...
+    if not worked:
+        print(url, "DID NOT WORK!!!")
+    sleep(3)
 
 while 1:
     1
-
-driver.quit()
