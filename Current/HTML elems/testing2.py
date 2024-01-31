577a496be0f4f0afbe2e3f2ad014dee68e90778e
Mitchell <mz2909@nyu.edu>
2024-01-30 19:39:51 -0500

    proxy can differential the type of resource

--- Current/HTML elems/testing2.py
+++ Current/HTML elems/testing2.py
@@ -22,24 +22,73 @@ driver = webdriver.Chrome(
     options=chrome_options
 )
 
-# Send a request to the specified URL
-url = 'https://www.uxmatters.com/'
-driver.get(url)
+websites = [
+    # "examplewebsite1.com",
+    # "webworldhub.net",
+    # "techtriumph.org",
+    # "stellarcode.io",
+    # "quantumgadget.com",
+    # "explorifyweb.com",
+    # "nexusplatform.org",
+    "velocityhub.com",
+    # "cosmicinsights.net",
+    # "datauniverse.io",
+    # "innovatexcellence.com",
+    # "pixelplanet.org",
+    # "synthwavehub.com",
+    # "infinitytech.io",
+    # "quantumspark.net",
+    # "dreamweaverlab.com",
+    # "vividvisions.org",
+    # "nebulaconnect.com",
+    # "digitalsunrise.io",
+    # "codecrafters.net"
+]
 
-# Wait for a moment to ensure all resources are loaded
-driver.implicitly_wait(5)
+for website in websites:
+    url = f"https://www.{website}"
+    driver.get(url)
 
+def content_eval(content_header):
+    stylesheet = ['text/css', 'application/css', 'application/x-css', 'text/plain',
+                  'text/html']
+    script = ['application/javascript', 'application/x-javascript', 'text/javascript', 'text/ecmascript',
+              'application/ecmascript']
+    images = ['image/jpeg', 'image/png', 'image/gif', 'image/bmp', 'image/webp', 'image/svg+xml', 'image/x-icon']
+    if content_header:
+        if any(elem in content_header for elem in stylesheet):
+            return "stylesheet"
+        if any(elem in content_header for elem in script):
+            return "script"
+        if any(elem in content_header for elem in images):
+            return "images"
+    return content_header
+
+types = []
+blocked = []
 # Output all fetched resources
 for request in driver.requests:
     if request.response:
         if request.response.status_code != 200:
-            print("something blocked")
+            blocked.append(request)
         else:
             print(f"URL: {request.url}")
             print(f"Method: {request.method}")
             print(f"Status Code: {request.response.status_code}")
-            print(f"Type: {request.response.headers['content-type']}")
+            content = content_eval(request.response.headers['content-type'])
+            print(f"Type: {content}          {request.response.headers['content-type']}")
             print("")
 
 # Close the WebDriver when done
 driver.quit()
+
+print(set(types))
+
+for i in blocked:
+    print(f"URL: {i.url}")
+    print(f"Method: {i.method}")
+    print(f"Status Code: {i.response.status_code}")
+    content = content_eval(i.response.headers['content-type'])
+    print(f"Type: {content}          {i.response.headers['content-type']}")
+    print("")
+
