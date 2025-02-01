a95627c844bab42e312767ea706350b87f172190
Racro <ritik.r@nyu.edu>
2025-01-31 20:18:53 -0500

    added argument support

--- 2. Resources (js)/blacklist_parser/mytest.js
+++ 2. Resources (js)/blacklist_parser/mytest.js
@@ -6,6 +6,10 @@ import {
     restart,
 } from './main.js';
 
+// Import yargs using ES modules
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
 (async ( ) => {
     // await enableWASM('.');
 
@@ -14,6 +18,19 @@ import {
     // }).then(pslRaw => {
     //     pslInit(pslRaw);
     // });
+
+    const argv = yargs(hideBin(process.argv))
+        .option('url', {
+            alias: 'u',
+            type: 'string',
+            description: 'The website URL',
+        })
+        .option('resource', {
+            alias: 'r',
+            type: 'string',
+            description: 'The resource',
+        })
+        .parse();
 
     const snfe = await Promise.all([
         readFile('./blacklists/easylist.txt', 'utf-8'),
@@ -27,26 +44,16 @@ import {
         ]);
     });
 
-    console.log("finished reading blacklists");
+    // console.log("finished reading blacklists");
 
     // Reuse filtering context: it's what uBO does
     const fctxt = new FilteringContext();
 
     // Tests
-    fctxt.setDocOriginFromURL('https://analytics.justuno.com/');
-    fctxt.setURL('https://analytics.justuno.com/');
-    // fctxt.setType('stylesheet');
-    if ( snfe.matchRequest(fctxt) !== 0 ) {
-        console.log(snfe.toLogData()['raw']);
-    }
-
-    fctxt.setURL('https://analytics.justuno.com/hi.txt');
-    // fctxt.setType('stylesheet');
-    if ( snfe.matchRequest(fctxt) !== 0 ) {
-        console.log(snfe.toLogData()['raw']);
-    }
-
-    fctxt.setURL('https://analytics.justuno.com/robots.txt');
+    // fctxt.setDocOriginFromURL('https://analytics.justuno.com/');
+    fctxt.setDocOriginFromURL(`${argv.url}`);
+    // fctxt.setURL('https://analytics.justuno.com/');
+    fctxt.setURL(`${argv.resource}`);
     // fctxt.setType('stylesheet');
     if ( snfe.matchRequest(fctxt) !== 0 ) {
         console.log(snfe.toLogData()['raw']);
