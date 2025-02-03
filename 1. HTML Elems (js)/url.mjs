ae13961f2da28af05ef4efff716dcebc93b9af65
Mitchell Zhou <mitche.zhou1@gmail.com>
2025-02-02 20:59:56 -0500

    need to integrate SS into make resource script

--- 1. HTML Elems (js)/url.mjs
+++ 1. HTML Elems (js)/url.mjs
@@ -3,8 +3,16 @@ export class Url {
       this.full_address = '';
       this.stripped_address = '';
       this.current_url = '';
+      this.key = '';
     }
   
+initialize(full_address){
+      this.full_address = full_address;
+      this.stripped_address = this.cleanUrl(full_address);
+      this.key = this.website_key(full_address);
+      this.current_url = '';
+    }
+
     cleanUrl(address) {
       let stripped_address = address.replace(/https?:\/\/www\./, '');
 
@@ -13,10 +21,25 @@ export class Url {
       return stripped_address;
     }
 
-    initialize(full_address){
-      this.full_address = full_address;
-      this.stripped_address = this.cleanUrl(full_address);
-      this.current_url = '';
-    }
+    website_key(full_address){
+      try {
+        var website_root = full_address.split("://")[1];
+        
+        if (website_root.includes('www')) {
+            website_root = website_root.split('.').slice(1).join('_');
+        } else {
+            website_root = website_root.split('.').join('_');
+        }
+    
+        website_root = website_root.replace(/\//g, '-');
+      } catch (error) {
+          console.log((full_address + "\n").repeat(100));
+          console.error(error);
+          website_root = full_address.replace(/\//g, '-');
+      }
+      return website_root;
+    };
+
+
   }
   
